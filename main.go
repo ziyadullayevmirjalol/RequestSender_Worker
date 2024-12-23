f9d273d0c0d373182b28224c3b20bfa258c58964
Mirjalo Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-12-23 16:28:22 +0500

    http server defined

--- main.go
+++ main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "time"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func sendGetRequest(url string) {
-    for {
-        resp, err := http.Get(url)
-        if err != nil {
-            fmt.Printf("An error occurred: %v\n", err)
-        } else {
-            fmt.Printf("Response status code: %d\n", resp.StatusCode)
-            resp.Body.Close()
-        }
-        time.Sleep(1 * time.Second)
-    }
+	for {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("An error occurred: %v\n", err)
+		} else {
+			fmt.Printf("Response status code: %d\n", resp.StatusCode)
+			resp.Body.Close()
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, world!")
 }
 
 func main() {
-    // example request endpoint
-    url := "https://booky-b5bu.onrender.com/api/book"
-    sendGetRequest(url)
+	url := "https://booky-b5bu.onrender.com/api/book"
+
+	go sendGetRequest(url)
+
+    http.HandleFunc("/", helloHandler)
+	fmt.Println("Server is listening on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
 }
